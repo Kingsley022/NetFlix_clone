ebee7cb6faaeb898d5b7ec38114ce91500ada5d9
Okereke Kingsley <kingsleyokereke911@gmail.com>
2023-05-19 13:57:12 +0100

    new commit

--- src/common/movierow.jsx
+++ src/common/movierow.jsx
@@ -49,7 +49,7 @@ const MovieRow = ({category, rowTitle, isLargeRow}) => {
     
     return (
         <div className= 'movieRow'>
-            <h2>{rowTitle || "Title"}</h2>
+            <h2>{rowTitle | "Title"}</h2>
             <div className= 'rows'>
                 {movies?.map( movie => (
                     <img src={`https://image.tmdb.org/t/p/w500/${isLargeRow ? movie?.poster_path : movie?.backdrop_path}`} 
